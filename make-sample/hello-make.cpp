8e9eb907508ec681a8c4473c44b05968ed373681
Michael Wright <mkwright@gmail.com>
2012-09-06 17:53:02 -0600

    Added env options

--- make-sample/hello-make.cpp
+++ make-sample/hello-make.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[], char* env[])
 {
+  std::cout << env[0] << std::endl;
   std::cout << "Hello, from make file" << std::endl;
 
   return 0;
