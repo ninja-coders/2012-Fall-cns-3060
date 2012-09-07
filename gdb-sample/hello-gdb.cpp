e9e1492aec65e96d176c2837b8ff5afff3be404b
Michael Wright <mkwright@gmail.com>
2012-09-06 18:19:36 -0600

    Fixed the program using gdb in class

--- gdb-sample/hello-gdb.cpp
+++ gdb-sample/hello-gdb.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[], char* env[])
 {
   std::cout << "Enter a numeric value: ";
   int number;
   std::cin >> number;
 
-  int sum = 0;
-  for (int i = 1; i < number; ++i) {
+  int sum = 1;
+  for (int i = 1; i <= number; ++i) {
     sum *= i;
   }
 
