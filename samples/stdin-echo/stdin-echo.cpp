53e40b7e99674cdc3f64fdd6e98b12f6771f6d2a
Michael Wright <mkwright@gmail.com>
2012-09-20 17:34:31 -0600

    Fixed an issue with not using the actual values in the buffer

--- samples/stdin-echo/stdin-echo.cpp
+++ samples/stdin-echo/stdin-echo.cpp
@@ -4,8 +4,8 @@ int main(int argc, char* argv[])
 {
   while (!ferror(stdin) && !feof(stdin)) {
     char buffer[25];
-    int read = fread(&buffer, sizeof(char), 25, stdin);
-    int write = fwrite(buffer, sizeof(char), 25, stdout);
+    int read = fread(buffer, sizeof(char), 25, stdin);
+    int write = fwrite(buffer, sizeof(char), read, stdout);
   }
 
   return 0;
